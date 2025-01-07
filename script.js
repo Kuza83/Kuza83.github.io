cfd81d162bb36a0d7ce903ad64f1c10da6dafc23
Kevin <kevin_maquaire@hotmail.com>
2025-01-08 00:58:05 +0100

    maj marti, autoplay audio

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const audioFile = document.getElementById('audio-file');
 const playBtn = document.getElementById('play-btn');
 
+audioFile.play()
+
 playBtn.addEventListener('click', () => {
   if (audioFile.paused) {
     audioFile.play();
-    playBtn.textContent = '⏸'; // Changer l'icône en pause
+    playBtn.textContent = '▶'; // Changer l'icône en pause
   } else {
     audioFile.pause();
-    playBtn.textContent = '▶'; // Changer l'icône en lecture
+    playBtn.textContent = '⏸'; // Changer l'icône en lecture
   }
 });
